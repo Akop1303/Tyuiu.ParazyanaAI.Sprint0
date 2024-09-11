9640b1c4cfef6c465caa43ba67fe32990dd6b820
Akop <parazyan_2001@mail.ru>
2024-09-12 00:40:31 +0500

    Task1

---
+++ Tyuiu.ParazyanAI.Sprint0.Task1.v0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ParazyanAI.Sprint0.Task1.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            string name = "Акоп";
+            int age = 23;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне  " + age + " года!");
+            Console.ReadKey();
+        }
+    }
+}

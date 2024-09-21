658e952b8375fed0050914743a59d686e8daa447
Tyuiu.ParazyanAI.Sprint0.Task1.v0 <parazyan_2001@mail.ru>
2024-09-21 23:18:00 +0500

    Task4

--- Tyuiu.ParazyanAI.Sprint0.Task4.V0.Lib/DataService.cs
+++ Tyuiu.ParazyanAI.Sprint0.Task4.V0.Lib/DataService.cs
@@ -13,16 +13,20 @@ namespace Tyuiu.ParazyanAI.Sprint0.Task4.V0.Lib
             return a + b; 
             }
 
+
         public static int Subtraction(int a, int b)
             { 
             return a - b; 
             }
 
+
         public static int Multiplication(int a, int b)
         { 
             return a * b; 
         }
-        public static int Division(int a, int b)
+
+
+        public static double Division(int a, int b)
         {
             return a / b;
         }
